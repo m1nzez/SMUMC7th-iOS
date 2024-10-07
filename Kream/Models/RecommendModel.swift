c93de925f9c139e7f203ab36b789c3c492ccb022
m1nzez <vivi020405@naver.com>
2024-10-08 00:13:16 +0900

    Add: Home의 CollectionView에 대한 cell 이미지 추가

---
+++ Kream/Models/RecommendModel.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendModel.swift
+//  Kream
+//
+//  Created by 김민지 on 10/8/24.
+//
+
+import Foundation
---
+++ Kream/Views/RecommendView.swift
@@ -0,0 +1,8 @@
+//
+//  RecommendView.swift
+//  Kream
+//
+//  Created by 김민지 on 10/7/24.
+//
+
+import Foundation
