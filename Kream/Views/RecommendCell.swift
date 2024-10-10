a4d4f89c6e96737574d7c697d31ad1c44ee63007
m1nzez <vivi020405@naver.com>
2024-10-11 06:38:11 +0900

    Add: RecommendView에 collectionCell 파일 추가

---
+++ Kream/Views/RecommendCell.swift
@@ -0,0 +1,18 @@
+//
+//  RecommendCell.swift
+//  Kream
+//
+//  Created by 김민지 on 10/11/24.
+//
+
+import UIKit
+
+class RecommendCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
