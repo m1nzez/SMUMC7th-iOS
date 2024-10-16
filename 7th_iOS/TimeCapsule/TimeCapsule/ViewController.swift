2616fcd4f489cb7a2ec32871611b0fa3c7a758c1
m1nzez <vivi020405@naver.com>
2024-10-16 09:39:15 +0900

    상단 디렉토리 생성

---
+++ 7th_iOS/TimeCapsule/TimeCapsule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeCapsule
+//
+//  Created by 김민지 on 10/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
